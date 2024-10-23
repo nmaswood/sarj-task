a97c64c57df53ac666c3b0abc4f1a4e49852babf
driiisdev <olayinkayakub01@gmail.com>
2024-10-23 19:15:54 +0100

    fix: mm

--- src/client/next.config.mjs
+++ src/client/next.config.mjs
@@ -6,18 +6,6 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  future: {
-    webpack5: true,
-  },
-  css: {
-    sourceMap: true,
-  },
-  image: {
-    formats: ['image/avif', 'image/webp'],
-  },
-  env: {
-    customKey: process.env.CUSTOM_VALUE,
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
