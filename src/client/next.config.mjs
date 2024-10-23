e0425d59d7c3c3ccc0df2d1f53aaf16dfa931edc
driiisdev <olayinkayakub01@gmail.com>
2024-10-23 18:19:22 +0100

    fix: next config

--- src/client/next.config.mjs
+++ src/client/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable module aliasing
-  moduleDirectories: ['node_modules', 'app'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  // Optional: Enable absolute imports for TypeScript
-  experimental: {
-    esmExternals: 'loose',
-  },
-  // Optional: Specify Webpack configuration (for advanced use cases)
-  // webpack: (config) => config,
-};
+const nextConfig = {};
 
 export default nextConfig;
