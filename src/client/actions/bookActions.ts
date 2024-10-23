84511cf386ff5714ee06ed144431d7bf6258cb97
driiisdev <olayinkayakub01@gmail.com>
2024-10-23 23:05:25 +0100

    fix: base

--- src/client/actions/bookActions.ts
+++ src/client/actions/bookActions.ts
@@ -1,7 +1,7 @@
 import { Book, SuccessResponse } from '@/lib/types/book';
 import { ApiError } from '@/lib/types/error';
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL?.replace(/\/$/, '/api/v1');
+const baseUrl = process.env.BASE_URL?.replace(/\/$/, '/api/v1');
 
 async function handleApiError(response: Response) {
   if (!response.ok) {
