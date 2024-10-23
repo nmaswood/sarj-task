925449726b41fb8710dbac05d9f3f5300cfad3dc
driiisdev <olayinkayakub01@gmail.com>
2024-10-23 19:25:14 +0100

    fix: web pack

--- src/client/postcss.config.mjs
+++ src/client/postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
